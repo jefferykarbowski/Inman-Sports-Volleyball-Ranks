1273cb05a6d32aec268dadc9677f3ae9c8a0b22d
jefferykarbowski <58263127+jefferykarbowski@users.noreply.github.com>
2022-02-16 19:16:24 -0500

    Plugin Boilerplate

---
+++ includes/class-inman sports volleyball ranks-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://inmansports.com
+ * @since      1.0.0
+ *
+ * @package    Inman_Sports_Volleyball_Ranks
+ * @subpackage Inman_Sports_Volleyball_Ranks/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Inman_Sports_Volleyball_Ranks
+ * @subpackage Inman_Sports_Volleyball_Ranks/includes
+ * @author     Andrew Inman <andy@inmansports.com>
+ */
+class Inman_Sports_Volleyball_Ranks_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-inman sports volleyball ranks-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://inmansports.com
+ * @since      1.0.0
+ *
+ * @package    Inman_Sports_Volleyball_Ranks
+ * @subpackage Inman_Sports_Volleyball_Ranks/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Inman_Sports_Volleyball_Ranks
+ * @subpackage Inman_Sports_Volleyball_Ranks/includes
+ * @author     Andrew Inman <andy@inmansports.com>
+ */
+class Inman_Sports_Volleyball_Ranks_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
