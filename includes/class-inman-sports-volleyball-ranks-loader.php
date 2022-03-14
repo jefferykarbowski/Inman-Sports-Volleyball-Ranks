a8d5a79d03038636e7e6a5dfc650c504e5f1d6e7
jefferykarbowski <58263127+jefferykarbowski@users.noreply.github.com>
2022-03-14 11:19:19 -0400

    Added shortcode ability

--- includes/class-inman-sports-volleyball-ranks-loader.php
+++ includes/class-inman-sports-volleyball-ranks-loader.php
@@ -41,6 +41,16 @@ class Inman_Sports_Volleyball_Ranks_Loader {
 	 */
 	protected $filters;
 
+    /**
+     * The array of shortcode registered with WordPress.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      array    $shortcodes    The shortcode registered with WordPress to fire when the plugin loads.
+     */
+    protected $shortcodes;
+
+
 	/**
 	 * Initialize the collections used to maintain the actions and filters.
 	 *
@@ -50,6 +60,7 @@ class Inman_Sports_Volleyball_Ranks_Loader {
 
 		$this->actions = array();
 		$this->filters = array();
+        $this->shortcodes = array();
 
 	}
 
@@ -80,6 +91,17 @@ class Inman_Sports_Volleyball_Ranks_Loader {
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
 	}
+
+    /**
+     * Add a new shortcode to the collection to be registered with WordPress.
+     *
+     * @since    1.0.0
+     * @param    string               $hook             The name of the WordPress shortcode that is being registered.
+     * @param    object               $component        A reference to the instance of the object on which the shortcode handler.
+     */
+    public function add_shortcode( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
+        $this->shortcodes = $this->add( $this->shortcodes, $hook, $component, $callback, $priority, $accepted_args );
+    }
 
 	/**
 	 * A utility function that is used to register the actions and hooks into a single
@@ -123,6 +145,10 @@ class Inman_Sports_Volleyball_Ranks_Loader {
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
 		}
+
+        foreach ( $this->shortcodes as $hook ) {
+            add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
+        }
 
 	}
 
